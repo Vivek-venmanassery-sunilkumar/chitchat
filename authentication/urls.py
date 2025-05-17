15f1798216c96cdf825d69ca41f8343158718ddd
kid <venmanassery.vivek@gmail.com>
2025-05-17 14:13:49 +0530

    routed the urls accordingly to signup and login views

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('signup/', Signup.as_view())
+    path('signup/', Signup.as_view()),
+    path('login/', Login.as_view()),
 ]
